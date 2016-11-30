62e0a7e5330ac2fb7192494c4caaf9732f7d9684
fatihtokus <fatih.tokus@gmail.com>
2016-11-30 21:17:20 +0000

    externalLinkTest passed

--- SimpleWebCrawler/src/test/java/org/crawler/WebCrawlerServiceImplTest.java
+++ SimpleWebCrawler/src/test/java/org/crawler/WebCrawlerServiceImplTest.java
@@ -1,5 +1,6 @@
 package org.crawler;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertThat;
 
@@ -7,15 +8,33 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
+
 public class WebCrawlerServiceImplTest {
+	
+	private WebCrawlerService webCrawlerService;
+	private Set<Page> pagesVisitedSoFar;
+	private Page processPage;
+	private String url;
+	
+	@Before
+	public void init() {
+		webCrawlerService =  new WebCrawlerServiceImpl();
+		url= "http://wiprodigital.com/";
+		pagesVisitedSoFar = new HashSet<Page>();
+		processPage = new Page(url);
+		webCrawlerService.parsePage(url, processPage, Optional.empty(), pagesVisitedSoFar);		
+	}	
+	
 	@Test
 	public void initialPageTest() {		
-		WebCrawlerService webCrawlerService =  new WebCrawlerServiceImpl();
-		String url= "http://wiprodigital.com/";
-		Set<Page> pageSetSoFar = new HashSet<Page>();
-		Page processPage = new Page(url);
-		webCrawlerService.parsePage(url, processPage, Optional.empty(), pageSetSoFar);	
-		assertThat(pageSetSoFar, hasItem(new Page("http://wiprodigital.com/")));		
+		assertThat(pagesVisitedSoFar, hasItem(new Page("http://wiprodigital.com/")));		
+	}
+	
+	@Test
+	public void externalLinkTest() {		
+		assertThat(processPage.getLinksToExternal(), hasItem("https://twitter.com/wiprodigital"));
+		assertThat(pagesVisitedSoFar, not(hasItem(new Page("https://twitter.com/wiprodigital"))));	
 	}
 }
